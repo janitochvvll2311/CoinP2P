8d8279c140adcc9959a25c6487d502da006ead37
CHAVA <janitochvvll2311@gmail.com>
2022-04-06 14:55:18 -0500

    Missing message bug fix

--- wwwroot/Scripts/Network.js
+++ wwwroot/Scripts/Network.js
@@ -70,9 +70,11 @@ async function send() {
     if (sockets.length > 0) {
         const message = `${inickname.value}> ${imessage.value}`;
         const signature = await sign(message);
+        print(message)
         var nonce = random();
         while (nonces.indexOf(nonce) >= 0)
             nonce = random();
+        nonces.push(nonce);
         const data = JSON.stringify({
             Nonce: nonce,
             Remote: ipublic.value,
